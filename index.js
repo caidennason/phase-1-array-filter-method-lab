1243121828e1cc1e90a75e236c1d971a28ed4c49
caiden nason <caidenbnason@gmail.com>
2022-04-29 12:13:33 -0400

    commit tree 1e204a382994f2178d8f9c919568998b526ae506

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arrayOfNames, name){
+    let answer = arrayOfNames.filter(ele => ele === name || ele.toLowerCase() === name.toLowerCase())
+    return answer
+    }
+
+    function fuzzyMatch(arrayOfNames, name){
+        let result = arrayOfNames.filter(ele => ele[0, 1] === name[0, 1])
+        return result
+    }
+
+    function matchName(objectOfDrivers, string){
+        let pain = objectOfDrivers.filter(nameEle => nameEle.name === string)
+        return pain
+    }
